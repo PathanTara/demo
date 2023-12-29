0a8e8a07092ab799d8e1652da0ab22d10f7bf9a4
PathanTara <124068110+PathanTara@users.noreply.github.com>
2023-12-29 11:09:08 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,7 +3,5 @@ st.set_page_config(page_title='cats')
 st.markdown("## Types of Cats")
 st.write("- Persian cat")
 persian_cat_image = r"C:\Users\HP\Desktop\Major Project\1.JPEG"
-st.image(persian_cat_image, caption='Persian cat', use_column_width=True)
 st.write("- White cat")
 white_cat_image = r"C:\Users\HP\Desktop\Major Project\2.JPEG"
-st.image(white_cat_image, caption='White cat', use_column_width=True)
