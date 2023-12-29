e87a08ebce3f11ee3afbc19b08ac8ffc26630700
PathanTara <124068110+PathanTara@users.noreply.github.com>
2023-12-29 11:26:46 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,8 +2,6 @@ import streamlit as st
 st.set_page_config(page_title='cats')
 st.markdown("## Types of Cats")
 st.write("- Persian cat")
-Persian_cat_image=".\1.jpg"
-st.image(Persian_cat_imahe)
+st.image(".\1.jpg)
 st.Write("- White cat")
-White_cat_image=".\2.jpg"
-st.image(white_cat_image)
+st.image(".\2.jpg)
